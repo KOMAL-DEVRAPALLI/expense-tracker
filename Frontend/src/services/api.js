1b3f6b71c10594d447c96fd962af5646d155bba5
Komal Devrapalli <komaldevlapeeli@gmail.com>
2026-06-05 14:00:16 +0530

    Production API URL

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+ baseURL: "https://expense-tracker-1-ewb1.onrender.com/api"
 });
 
 export default api;
