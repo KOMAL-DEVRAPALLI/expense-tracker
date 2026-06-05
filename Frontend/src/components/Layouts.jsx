f126b65b5b6cb5f5ae65b918fe05565ac82124d3
Komal Devrapalli <komaldevlapeeli@gmail.com>
2026-06-05 14:12:57 +0530

    Production API URL

--- Frontend/src/components/Layouts.jsx
+++ Frontend/src/components/Layouts.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
+
 function Layout({ children }) {
   return (
     <div className="layout">
-
       <aside className="sidebar">
         <div className="logo">
           <h2>💰 Expense Tracker</h2>
@@ -9,17 +10,16 @@ function Layout({ children }) {
         </div>
 
         <nav>
-          <a href="/">Summary</a>
-          <a href="/deposits">Deposits</a>
-          <a href="/expenses">Expenses</a>
-          <a href="/members">Members</a>
+          <Link to="/">Summary</Link>
+          <Link to="/deposits">Deposits</Link>
+          <Link to="/expenses">Expenses</Link>
+          <Link to="/members">Members</Link>
         </nav>
       </aside>
 
       <main className="content">
         {children}
       </main>
-
     </div>
   );
 }
