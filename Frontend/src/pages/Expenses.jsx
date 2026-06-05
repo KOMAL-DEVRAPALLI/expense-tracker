c05261e7bbd4d5ad3501140f4a74b43ffb1a3c13
Komal Devrapalli <komaldevlapeeli@gmail.com>
2026-06-05 16:25:14 +0530

    ADD delete function

--- Frontend/src/pages/Expenses.jsx
+++ Frontend/src/pages/Expenses.jsx
@@ -40,74 +40,78 @@ fetchExpenses()
     }
   };
 
-  return (
-    <div>
-      <h1>Expenses</h1>
+ return (
+  <div>
+    <h1>Expenses</h1>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Expense Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Expense Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
 
-        <input
-          type="number"
-          placeholder="Amount"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
+      <input
+        type="number"
+        placeholder="Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
 
-        <button type="submit">Add Expense</button>
-      </form>
-      {message && (
-  <p className="success-message">
-    {message}
-  </p>
-)}
+      <button type="submit">
+        Add Expense
+      </button>
+    </form>
+
+    {message && (
+      <p className="success-message">
+        {message}
+      </p>
+    )}
+
+    <br />
+
+    <div className="table-card">
+      <div className="table-header">
+        <h3>Expense History</h3>
+      </div>
+
+      {expenses.length === 0 ? (
+        <p style={{ padding: "20px" }}>
+          No expenses found
+        </p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Amount</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {expenses.map((expense) => (
+              <tr key={expense._id}>
+                <td>{expense.title}</td>
+
+                <td>
+                  ₹{expense.amount}
+                </td>
+
+                <td>
+                  {new Date(
+                    expense.createdAt
+                  ).toLocaleDateString()}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
-    
-  );
-  <div className="table-card">
-  <div className="table-header">
-    <h3>Expense History</h3>
   </div>
-
-  {expenses.length === 0 ? (
-    <p style={{ padding: "20px" }}>
-      No expenses found
-    </p>
-  ) : (
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Amount</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {expenses.map((expense) => (
-          <tr key={expense._id}>
-            <td>{expense.title}</td>
-
-            <td>
-              ₹{expense.amount}
-            </td>
-
-            <td>
-              {new Date(
-                expense.createdAt
-              ).toLocaleDateString()}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  )}
-</div>
+);
 }
-
 export default Expenses;
